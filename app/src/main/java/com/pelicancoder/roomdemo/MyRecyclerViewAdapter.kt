3ce849c70f7147f68d0095a4be5349aa27a04ca1
Philip Stephens <plusgforce@gmail.com>
2023-05-19 05:01:48 -0600

    Initial commit

---
+++ app/src/main/java/com/pelicancoder/roomdemo/MyRecyclerViewAdapter.kt
@@ -0,0 +1,36 @@
+package com.pelicancoder.roomdemo
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.pelicancoder.roomdemo.databinding.ListItemBinding
+import com.pelicancoder.roomdemo.db.Subscriber
+
+class MyRecyclerViewAdapter(private val subscribersList: List<Subscriber>):
+    RecyclerView.Adapter<MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding: ListItemBinding =
+            DataBindingUtil.inflate(layoutInflater, R.layout.list_item, parent, false)
+        return MyViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bind(subscribersList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return subscribersList.size
+    }
+}
+
+class MyViewHolder(val binding: ListItemBinding): RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(subscriber: Subscriber) {
+        binding.nameTextView.text = subscriber.name
+        binding.emailTextView.text = subscriber.email
+    }
+}
